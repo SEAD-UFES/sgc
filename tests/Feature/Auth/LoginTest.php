275af03d8d01961d544402a956365fb7312b82fa
Eduardo França <edox86@gmail.com>
2021-07-02 14:58:02 -0400

    Login Page Test
    Tests route, view and login form fields in page.

---
+++ tests/Feature/Auth/LoginTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\Feature\Auth;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class LoginTest extends TestCase
+{
+  /**
+   * A basic feature test example.
+   *
+   * @return void
+   */
+  public function test_user_can_view_a_login_form()
+  {
+    $response = $this->get('/');
+
+    $response->assertSuccessful();
+
+    $response->assertViewIs('login');
+
+    $response->assertSee('email');
+    $response->assertSee('password');
+    $response->assertSee('submit');
+  }
+}
