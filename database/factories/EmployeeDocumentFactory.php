0d59b668e9bef4c6ea038fc41f19a02008f43a07
Eduardo França <edox86@gmail.com>
2021-07-22 21:59:00 -0400

    Factory for Employee Documents

--- database/factories/EmployeeDocumentFactory.php
+++ database/factories/EmployeeDocumentFactory.php
@@ -3,6 +3,8 @@
 namespace Database\Factories;
 
 use App\Models\EmployeeDocument;
+use App\Models\DocumentType;
+use App\Models\Employee;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class EmployeeDocumentFactory extends Factory
@@ -21,8 +23,21 @@ class EmployeeDocumentFactory extends Factory
      */
     public function definition()
     {
+        $fileBase64 = "JVBERi0xLjIgCjkgMCBvYmoKPDwKPj4Kc3RyZWFtCkJULyA5IFRmKF"
+            . "Rlc3QpJyBFVAplbmRzdHJlYW0KZW5kb2JqCjQgMCBvYmoKPDwKL1R5cGUgL1Bh"
+            . "Z2UKL1BhcmVudCA1IDAgUgovQ29udGVudHMgOSAwIFIKPj4KZW5kb2JqCjUgMC"
+            . "BvYmoKPDwKL0tpZHMgWzQgMCBSIF0KL0NvdW50IDEKL1R5cGUgL1BhZ2VzCi9N"
+            . "ZWRpYUJveCBbIDAgMCA5OSA5IF0KPj4KZW5kb2JqCjMgMCBvYmoKPDwKL1BhZ2"
+            . "VzIDUgMCBSCi9UeXBlIC9DYXRhbG9nCj4+CmVuZG9iagp0cmFpbGVyCjw8Ci9S"
+            . "b290IDMgMCBSCj4+CiUlRU9G";
+
         return [
-            //
+          'document_type_id' => DocumentType::factory(),
+          'employee_id' => Employee::factory(),
+          'original_name' => $this->faker->lexify('????????????????.pdf'),
+          'file_data' => $fileBase64,
+          'created_at' => now(), 
+          'updated_at' => now(),
         ];
     }
 }
