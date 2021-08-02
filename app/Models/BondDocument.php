0136b555d82dc5d207495fa5b52f768c797bcf7b
annecchini <fernando.void@gmail.com>
2021-08-02 10:19:47 -0300

    add sortable_on_bond_documents (model, controller, blade) change custom sort method - vinculo - on bond_document model little change on employee

--- app/Models/BondDocument.php
+++ app/Models/BondDocument.php
@@ -4,10 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Kyslik\ColumnSortable\Sortable;
 
 class BondDocument extends Model
 {
     use HasFactory;
+    use Sortable;
 
     protected $table = 'bond_documents';
 
@@ -15,6 +17,24 @@ class BondDocument extends Model
         'original_name',
         'file_data',
     ];
+
+    public $sortable = [
+        'id',
+        'original_name',
+        'created_at',
+        'updated_at'
+    ];
+
+    //sobrepor metodo de ordenação para vinculo (bond->emplyee->name) no sortable
+    public function vinculoSortable($query, $direction)
+    {
+        $query = $query
+            ->join('bonds', 'bond_documents.bond_id', '=', 'bonds.id')
+            ->join('employees', 'bonds.employee_id', '=', 'employees.id')
+            ->orderBy('employees.name', $direction)
+            ->select('bond_documents.*');
+        return $query;
+    }
 
     public function documentType()
     {
