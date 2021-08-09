e485376a7b6c9a0b81daf737cf4b8a27e9e16ff8
Marco Aurelio Cardoso <marcoaurelio.cardoso@gmail.com>
2021-08-09 19:44:56 -0300

    Login form and navbar adjusts
    Applied Bootstrap on Login form and navbar adjusts

--- resources/views/layouts/parcialHeader.blade.php
+++ resources/views/layouts/parcialHeader.blade.php
@@ -3,12 +3,13 @@
         <a class="navbar-brand" href="#"><img src="{{ asset('/sead.png') }}" alt="" width="30" {{-- height="24" --}}
                 class="d-inline-block align-text-top">
             SGC</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            @if (Auth::check())
+        @if (Auth::check())
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item"><a class="nav-link active" aria-current="page"
                             href="{{ route('home') }}">Home</a></li>
@@ -43,7 +44,7 @@
                         [<a href="{{ route('auth.logout') }}">Sair</a>]</li>
 
                 </ul>
-            @endif
-        </div>
+            </div>
+        @endif
     </div>
 </nav>
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,33 +3,46 @@
 @section('title', 'Login')
 
 @section('content')
-    <section>
+    {{-- <section>
         <h2>Login</h2>
-    </section>
+    </section> --}}
     <section id="pageContent">
-        <main role="main">
-            <form action={{ route('auth.login') }} method="POST">
-                @csrf
-                E-Mail*: <input name="email" type="text" placeholder="Email" value="{{ old('email') }}" />
-                @error('email')
-                    <div class="error">&gt; {{ $message }}</div>
-                @enderror
-                <br /><br />
-                Senha*: <input name="password" type="password" placeholder="Senha" />
-                @error('password')
-                    <div class="error">&gt; {{ $message }}</div>
-                @enderror
-                <br /><br />
-                <button type="submit">Login</button>
-                @error('noAuth')
-                    <div class="error">&gt; {{ $message }}</div>
-                @enderror
-                <br /><br />
-            </form>
-            <code>
-                Usuário de teste: admin@ufes.br<br />
-                Senha: senha123
-            </code><br /><br />
+        <main role="main" class="">
+            <div class="row justify-content-center">
+                <div class="col-sm-8 col-md-6 col-lg-5 col-xl-4 col-xxl-3">
+                    <div class="border border-2 rounded-3 my-3 p-3">
+                        <form action={{ route('auth.login') }} method="POST">
+                            <h3>Login</h3>
+                            @csrf
+                            <div class="mb-3">
+                                <label for="inputEmail1" class="form-label">E-Mail*</label>
+                                <input name="email" type="text" id="inputEmail1" class="form-control" placeholder="Email"
+                                    value="{{ old('email') }}" />
+                                @error('email')
+                                    <div class="error">&gt; {{ $message }}</div>
+                                @enderror
+                            </div>
+                            <div class="mb-3">
+                                <label for="inputSenha1" class="form-label">Senha*</label>
+                                <input name="password" type="password" id="inputSenha1" class="form-control"
+                                    placeholder="Senha" />
+                                @error('password')
+                                    <div class="error">&gt; {{ $message }}</div>
+                                @enderror
+                            </div>
+                            <button type="submit" class="btn btn-primary">Login</button>
+                            @error('noAuth')
+                                <div class="error">&gt; {{ $message }}</div>
+                            @enderror
+                        </form>
+                        <br /><br />
+                        <code>
+                            Usuário de teste: admin@ufes.br<br />
+                            Senha: senha123
+                        </code>
+                    </div>
+                </div>
+            </div>
         </main>
     </section>
 @endsection
