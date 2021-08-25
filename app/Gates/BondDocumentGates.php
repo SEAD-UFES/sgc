0e8e2acdd24dde9c8fed14a7b83ce748af101f11
annecchini <fernando.void@gmail.com>
2021-08-25 14:12:21 -0300

    Add coords on rights gate

--- app/Gates/BondDocumentGates.php
+++ app/Gates/BondDocumentGates.php
@@ -9,8 +9,11 @@ class BondDocumentGates
     public static function define()
     {
         Gate::define('bondDocument-rights', function ($user) {
-            //who can do it.
-            if (Gate::forUser($user)->any(['is-Adm-global', 'is-Dir-global', 'is-Ass-global', 'is-Sec-global', 'is-Gra-global'])) return true;
+            //who can do it (global).
+            if (Gate::forUser($user)->any(['is-Adm-global', 'is-Dir-global', 'is-Ass-global', 'is-Sec-global', 'is-Ldi-global'])) return true;
+
+            //coords of any course
+            if (Gate::forUser($user)->any(['isGra'])) return true;
 
             //no permission
             return false;
