ea13991078d0ed6757471e2fbe058a1df04da517
annecchini <fernando.void@gmail.com>
2021-08-27 15:43:44 -0300

    fix gate name isCor to is Coord

--- resources/views/layouts/parcialHeader.blade.php
+++ resources/views/layouts/parcialHeader.blade.php
@@ -12,7 +12,7 @@
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item"><a class="nav-link active" aria-current="page" href="{{ route('home') }}">Home</a></li>
 
-                    @canany(['isAdm', 'isDir', 'isAss', 'isSec', 'isCor'])
+                    @canany(['isAdm', 'isDir', 'isAss', 'isSec', 'isCoord'])
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toogle" href="" {{-- href="{{ route('employee') }}" --}} id="navbarSupportedContentMenuLink1" role="button" data-bs-toggle="dropdown" aria-expanded="false">Colaboradores</a>
                             <ul class="dropdown-menu" aria-labelledby="navbarSupportedContentMenuLink1">
@@ -44,7 +44,7 @@
                         </ul>
                     </li>
 
-                    @canany(['isAdm', 'isDir', 'isAss', 'isSec', 'isCor'])
+                    @canany(['isAdm', 'isDir', 'isAss', 'isSec', 'isCoord'])
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toogle" href="" {{-- href="{{ route('system') }}" --}} id="navbarSupportedContentMenuLink4" role="button" data-bs-toggle="dropdown" aria-expanded="false">Sistema</a>
                             <ul class="dropdown-menu" aria-labelledby="navbarSupportedContentMenuLink4">
