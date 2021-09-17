429ae870ae80461e7751933a3b50b3c5464829dc
annecchini <fernando.void@gmail.com>
2021-09-17 20:05:09 -0300

    bond_interface_review - Categorias bem definidas - categorias colpsaveis - titulos em negrito - mensagens de 0 itens em listas - botões de operação tem um padrão e navegação - opções de review integradas em uma categoria - dados de review migradaos para essa categoria - liberdade de click do usuário (sim/nao) - mudança de cor dinamica do review OBS1: levarl hasRights para o controller OBS2: ordenar e filtrar aqui pode dar ruim.

--- resources/views/bond/show.blade.php
+++ resources/views/bond/show.blade.php
@@ -17,63 +17,118 @@
         <div class="row justify-content-center">
             <div class="col-xl-10 col-xxl-8">
                 @include('_components.alerts')
+
                 @component('bond.componentBondDetails', compact('bond'))@endcomponent
-                <br />
-                <h4>> Documentos</h4>
-                @component('bond.document.componentList', compact('documents'))@endcomponent
-                <a href="{{ route('bonds.document.massdownload', $bond) }}" 
-                    class="btn btn-primary btn-sm">&nbsp;&#8627; Fazer o download de todos os documentos do
-                    vínculo (zip)</a>
-                <br /><br /><br />
 
-                @php
-                    $hasRights = $documents->where('document_type_id', App\Models\DocumentType::where('name', 'Ficha de Inscrição - Termos e Licença')->first()->id)->count() > 0;
-                @endphp
+                <div class="card mb-3">
+                    <div class="card-header" data-bs-toggle="collapse" href="#bondDocumentListContent" role="button" aria-expanded="true" aria-controls="bondDocumentListContent">
+                        <h4 class='mb-0'>Documentos do vínculo</h4>
+                    </div>
+                    <div class="collapse show" id="bondDocumentListContent" >
+                        <div class="card-body">
+                            @if($documents->count() > 0)
+                                @component('bond.document.componentList', compact('documents'))@endcomponent
+                                <a href="{{ route('bonds.document.massdownload', $bond) }}" class="btn btn-primary btn-sm">
+                                    &nbsp;&#8627; Fazer o download de todos os documentos do vínculo (zip)
+                                </a>
+                            @else
+                                <p class="mb-0">O vínculo não possui documentos cadastrados.</p>
+                            @endif
+                        </div>
+                    </div>
+                </div>
 
-                @can('bond-review')
-                    <fieldset class="bg-warning px-2 py-2">
-                        <h4>> Revisão</h4>
-                        <form name="{{ 'formReview' . $bond->id }}" action="{{ route('bonds.review', $bond) }}" method="POST">
-                            @csrf
-                            <label class="form-label">Impedido:</label><br />
-                            <div class="mb-3 form-check">
-                                <input class="form-check-input" type="radio" id="sim" name="impediment" value="1"
-                                    {{ $bond->impediment == '1' ? 'checked' : '' }}
-                                    onclick="document.getElementById('impedimentDescription').disabled=false;">
-                                <label for="sim" class="form-check-label">Sim</label><br>
-                                <input class="form-check-input" type="radio" id="nao" name="impediment" value="0"
-                                    {{ $bond->impediment == '0' ? 'checked' : '' }}
-                                    onclick="document.getElementById('impedimentDescription').disabled=true;"
-                                    {{ !$hasRights ? 'disabled' : '' }}>
-                                <label for="nao" class="form-check-label">Não*</label><br>
-                                <p>* Para a aprovação de um vínculo é necessário ter o documento 'Ficha de Inscrição - Termos e Licença'</p>
+                @canany(['bond-review', 'bond-requestReview'])
+
+                    @php
+                        $hasRights = $documents->where('document_type_id', App\Models\DocumentType::where('name', 'Ficha de Inscrição - Termos e Licença')->first()->id)->count() > 0;
+                    @endphp
+
+                    <div class="card {{ $bond->impediment == 0 ? 'border-success' : 'border-warning'}}  mb-3">
+                        <div class="card-header {{ $bond->impediment == 0 ? 'bg-success' : 'bg-warning'}}" data-bs-toggle="collapse" href="#bondReviewContent" role="button" aria-expanded="true" aria-controls="bondReviewContent">
+                            <h4 class='mb-0'>Revisão do vínculo</h4>
+                        </div>
+
+                        <div class="collapse show" id="bondReviewContent" >
+                            <div class="card-body">
+
+                                @can('bond-requestReview')
+
+                                    <div class="mb-2 row">
+                                        <div class="col-sm-4 col-lg-3"><strong>Última revisão:</strong></div>
+                                        <div class="col-sm-8 col-lg-9">{{ ($bond->uaba_checked_at != null) ? \Carbon\Carbon::parse($bond->uaba_checked_at)->isoFormat('DD/MM/Y hh:mm') : '-' }}</div>
+                                    </div>
+
+                                    <div class="mb-2 row">
+                                        <div class="col-sm-4 col-lg-3">
+                                            <strong>Status do vínculo:</strong>
+                                        </div>
+                                        <div class="col-sm-8 col-lg-9">
+                                            {{ $bond->impediment == 0 ? 'Sem pendências' : 'Impedido'}}
+                                            {{ $bond->impediment == 0 && !$hasRights ? ' - OBS: Sem o documento "Ficha de Inscrição - Termos e Licença" o vínculo permanecerá impedido.' : ''}}
+                                        </div>
+                                    </div>
+
+                                    @if ($bond->impediment == '1')
+                                        <div class="mb-2 row">
+                                            <div class="col-sm-4 col-lg-3">
+                                                <strong>Descrição do impedimento:</strong>
+                                            </div>
+                                            <div class="col-sm-8 col-lg-9">
+                                                {{ $bond->impediment == '1' ? $bond->impediment_description : '' }}
+                                                <br>
+                                                {{ !$hasRights ? 'OBS: Sem o documento "Ficha de Inscrição - Termos e Licença" o vínculo permanecerá impedido.' : ''}}
+                                            </div>
+                                        </div>
+                                    @endif
+
+                                    <a href="{{ route('bonds.requestReview', $bond->id) }}" class="btn btn-primary btn-sm">Enviar notificação de revisão de vínculo</a>
+                                @endcan
+
+                                @can(['bond-review', 'bond-requestReview'])
+                                    <hr>
+                                @endcan
+
+                                @can('bond-review')
+                                        <form name="{{ 'formReview' . $bond->id }}" action="{{ route('bonds.review', $bond) }}" method="POST">
+                                            @csrf
+                                            <label class="form-label">Impedido:</label>
+                                            <div class="mb-3 form-check">
+                                                <input class="form-check-input" type="radio" id="sim" name="impediment" value="1"
+                                                    {{ $bond->impediment == '1' ? 'checked' : '' }}
+                                                    onclick="document.getElementById('impedimentDescription').disabled=false;">
+                                                <label for="sim" class="form-check-label">
+                                                    Sim
+                                                </label>
+                                                <br>
+                                                
+                                                <input class="form-check-input" type="radio" id="nao" name="impediment" value="0"
+                                                    {{ $bond->impediment == '0' ? 'checked' : '' }}
+                                                    onclick="document.getElementById('impedimentDescription').disabled=true;">
+                                                <label for="nao" class="form-check-label">
+                                                    Não (Sem o documento "Ficha de Inscrição - Termos e Licença" o vínculo permanecerá impedido.)
+                                                </label>
+                                            </div>
+                                            <div class="mb-3">
+                                                <label for="impedimentDescription" class="form-label">Motivo do impedimento:</label><br />
+                                                <textarea class="form-control" id="impedimentDescription" name="impedimentDescription"
+                                                    rows="4">{{ $bond->impediment_description ?? '' }}</textarea>
+                                                <script>
+                                                    if ({{ $bond->impediment }} == '0')
+                                                        document.getElementById('impedimentDescription').disabled = true;
+                                                </script>
+                                            </div>
+                                            <input type="submit" value="Revisar" class="btn btn-primary btn-sm my-2 mx-1">
+                                        </form>
+                                @endcan
+
                             </div>
-                            <div class="mb-3">
-                                <label for="impedimentDescription" class="form-label">Motivo do impedimento:</label><br />
-                                <textarea class="form-control" id="impedimentDescription" name="impedimentDescription"
-                                    rows="4">{{ $bond->impediment_description ?? '' }}</textarea>
-                                <script>
-                                    if ({{ $bond->impediment }} == '0')
-                                        document.getElementById('impedimentDescription').disabled = true;
-                                </script>
-                            </div>
-                            <input type="submit" value="Revisar" class="btn btn-primary my-2 mx-1">
-                        </form>
-                    </fieldset><br />
-                @endcan
-
-                @can('bond-requestReview')
-                    @if ($bond->impediment == '1')
-                        <fieldset class="bg-warning px-2 py-2">
-                            <h4>> Nova Revisão</h4>
-                            <a href="{{ route('bonds.requestReview', $bond->id) }}" class="btn btn-primary my-2 mx-1">Solicitar
-                                nova revisão</a>
-                        </fieldset><br />
-                    @endif
-                @endcan
+                        </div>
+                    </div>
+                @endcanany
 
                 <button type="button" onclick="history.back()" class="btn btn-secondary">Voltar</button>
-                <br /><br />
+                <br />
             </div>
         </div>
     </main>
