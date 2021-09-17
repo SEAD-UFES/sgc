b8a2b421889b821c301d675f070bcc980d49f689
annecchini <fernando.void@gmail.com>
2021-09-17 20:05:09 -0300

    bond_interface_review - Categorias bem definidas - categorias colpsaveis - titulos em negrito - mensagens de 0 itens em listas - botões de operação tem um padrão e navegação - opções de review integradas em uma categoria - dados de review migradaos para essa categoria - liberdade de click do usuário (sim/nao) - mudança de cor dinamica do review OBS1: levarl hasRights para o controller OBS2: ordenar e filtrar aqui pode dar ruim.

--- resources/views/bond/componentBondDetails.blade.php
+++ resources/views/bond/componentBondDetails.blade.php
@@ -1,58 +1,81 @@
-<h4>> Dados Pessoais</h4>
-<div class="mb-2 row">
-    <div class="col-sm-4 col-lg-3">Nome:</div>
-    <div class="col-sm-8 col-lg-9">{{ $bond->employee->name ?? '-' }}</div>
+<div class="card mb-3">
+    <div class="card-header" data-bs-toggle="collapse" href="#employeeDataContent" role="button" aria-expanded="true" aria-controls="employeeDataContent">
+        <h4 class='mb-0'>Dados do colaborador</h4>
+    </div>
+    
+    <div class="collapse show" id="employeeDataContent" >
+        <div class="card-body" >
+            <div class="mb-2 row">
+                <div class="col-sm-4 col-lg-3"><strong>Nome:</strong></div>
+                <div class="col-sm-8 col-lg-9">{{ $bond->employee->name ?? '-' }}</div>
+            </div>
+            <div class="mb-2 row">
+                <div class="col-sm-4 col-lg-3"><strong>Cidade:</strong></div>
+                <div class="col-sm-8 col-lg-9">{{ $bond->employee->address_city ?? '-' }}</div>
+            </div>
+            <div class="mb-2 row">
+                <div class="col-sm-4 col-lg-3"><strong>Email:</strong></div>
+                <div class="col-sm-8 col-lg-9">{{ $bond->employee->email ?? '-' }}</div>
+            </div>
+
+            <div class="">
+                @can('employee-show')
+                <a href="{{ route('employees.show', $bond->employee->id) }}" class="btn btn-primary btn-sm">
+                    <i class="bi-eye-fill"></i> Consultar colaborador
+                </a>&nbsp;
+                @endcan
+            </div>
+        </div> 
+    </div>
 </div>
-<div class="mb-2 row">
-    <div class="col-sm-4 col-lg-3">Cidade:</div>
-    <div class="col-sm-8 col-lg-9">{{ $bond->employee->address_city ?? '-' }}</div>
-</div>
-<div class="mb-2 row">
-    <div class="col-sm-4 col-lg-3">Email:</div>
-    <div class="col-sm-8 col-lg-9">{{ $bond->employee->email ?? '-' }}</div>
-</div>
-<h4>> Dados do vínculo</h4>
-<div class="mb-2 row">
-    <div class="col-sm-4 col-lg-3">Atribuição:</div>
-    <div class="col-sm-8 col-lg-9">{{ $bond->role->name ?? '-' }}</div>
-</div>
-<div class="mb-2 row">
-    <div class="col-sm-4 col-lg-3">Curso:</div>
-    <div class="col-sm-8 col-lg-9">{{ $bond->course->name ?? '-' }}</div>
-</div>
-<div class="mb-2 row">
-    <div class="col-sm-4 col-lg-3">Polo:</div>
-    <div class="col-sm-8 col-lg-9">{{ $bond->pole->name ?? '-' }}</div>
-</div>
-<div class="mb-2 row">
-    <div class="col-sm-4 col-lg-3">Criado em:</div>
-    <div class="col-sm-8 col-lg-9">{{ ($bond->created_at != null) ? \Carbon\Carbon::parse($bond->created_at)->isoFormat('DD/MM/Y hh:mm') : '-' }}</div>
-</div>
-<div class="mb-2 row">
-    <div class="col-sm-4 col-lg-3">Início:</div>
-    <div class="col-sm-8 col-lg-9">{{ ($bond->begin != null) ? \Carbon\Carbon::parse($bond->begin)->isoFormat('DD/MM/Y') : '-' }}</div>
-</div>
-<div class="mb-2 row">
-    <div class="col-sm-4 col-lg-3">Fim:</div>
-    <div class="col-sm-8 col-lg-9">{{ ($bond->end != null) ? \Carbon\Carbon::parse($bond->end)->isoFormat('DD/MM/Y') : '-' }}</div>
-</div>
-<div class="mb-2 row">
-    <div class="col-sm-4 col-lg-3">Voluntário:</div>
-    <div class="col-sm-8 col-lg-9">{{ $bond->volunteer === 1 ? 'Sim' : 'Não' }}</div>
-</div>
-<div class="mb-2 row">
-    <div class="col-sm-4 col-lg-3">Encerrado em:</div>
-    <div class="col-sm-8 col-lg-9">{{ ($bond->terminated_at != null) ? \Carbon\Carbon::parse($bond->terminated_at)->isoFormat('DD/MM/Y hh:mm') : '-' }}</div>
-</div>
-<div class="mb-2 row">
-    <div class="col-sm-4 col-lg-3">Impedido:</div>
-    <div class="col-sm-8 col-lg-9">{{ $bond->impediment === 1 ? 'Sim' : 'Não' }}</div>
-</div>
-<div class="mb-2 row">
-    <div class="col-sm-4 col-lg-3">Descrição do Impedimento:</div>
-    <div class="col-sm-8 col-lg-9">{{ $bond->impediment_description ?? '-' }}</div>
-</div>
-<div class="mb-2 row">
-    <div class="col-sm-4 col-lg-3">Revisado em:</div>
-    <div class="col-sm-8 col-lg-9">{{ ($bond->uaba_checked_at != null) ? \Carbon\Carbon::parse($bond->uaba_checked_at)->isoFormat('DD/MM/Y hh:mm') : '-' }}</div>
+
+<div class="card mb-3">
+    <div class="card-header" data-bs-toggle="collapse" href="#bondDataContent" role="button" aria-expanded="true" aria-controls="bondDataContent">
+        <h4 class='mb-0'>Dados do vínculo</h4>
+    </div>
+
+    <div class="collapse show" id="bondDataContent" >
+        <div class="card-body">
+            <div class="mb-2 row">
+                <div class="col-sm-4 col-lg-3"><strong>Atribuição:</strong></div>
+                <div class="col-sm-8 col-lg-9">{{ $bond->role->name ?? '-' }}</div>
+            </div>
+            <div class="mb-2 row">
+                <div class="col-sm-4 col-lg-3"><strong>Curso:</strong></div>
+                <div class="col-sm-8 col-lg-9">{{ $bond->course->name ?? '-' }}</div>
+            </div>
+            <div class="mb-2 row">
+                <div class="col-sm-4 col-lg-3"><strong>Polo:</strong></div>
+                <div class="col-sm-8 col-lg-9">{{ $bond->pole->name ?? '-' }}</div>
+            </div>
+            <div class="mb-2 row">
+                <div class="col-sm-4 col-lg-3"><strong>Criado em:</strong></div>
+                <div class="col-sm-8 col-lg-9">{{ ($bond->created_at != null) ? \Carbon\Carbon::parse($bond->created_at)->isoFormat('DD/MM/Y hh:mm') : '-' }}</div>
+            </div>
+            <div class="mb-2 row">
+                <div class="col-sm-4 col-lg-3"><strong>Início:</strong></div>
+                <div class="col-sm-8 col-lg-9">{{ ($bond->begin != null) ? \Carbon\Carbon::parse($bond->begin)->isoFormat('DD/MM/Y') : '-' }}</div>
+            </div>
+            <div class="mb-2 row">
+                <div class="col-sm-4 col-lg-3"><strong>Fim:</strong></div>
+                <div class="col-sm-8 col-lg-9">{{ ($bond->end != null) ? \Carbon\Carbon::parse($bond->end)->isoFormat('DD/MM/Y') : '-' }}</div>
+            </div>
+            <div class="mb-2 row">
+                <div class="col-sm-4 col-lg-3"><strong>Voluntário:</strong></div>
+                <div class="col-sm-8 col-lg-9">{{ $bond->volunteer === 1 ? 'Sim' : 'Não' }}</div>
+            </div>
+            <div class="mb-2 row">
+                <div class="col-sm-4 col-lg-3"><strong>Encerrado em:</strong></div>
+                <div class="col-sm-8 col-lg-9">{{ ($bond->terminated_at != null) ? \Carbon\Carbon::parse($bond->terminated_at)->isoFormat('DD/MM/Y hh:mm') : '-' }}</div>
+            </div>
+
+            <div class="">
+                @can('bond-update', $bond)
+                    <a href="{{ route('bonds.edit', $bond->id) }}" data-bs-toggle="tooltip" title="Editar vínculo" class="btn btn-primary btn-sm">
+                        <i class="bi-pencil-fill"></i> Editar vínculo
+                    </a>&nbsp;
+                @endcan
+            </div>
+        </div>
+    </div>
 </div>
