db26e2db2fbb6ee2b9eb36d4a5251391665bd2a1
Marco Aurelio Cardoso <marcoaurelio.cardoso@gmail.com>
2022-01-22 04:22:23 -0300

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,9 +14,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        if (env('APP_ENV') == 'production') {
-            $this->app['request']->server->set('HTTPS', true);
-        }
+        $this->app['request']->server->set('HTTPS', true);
     }
 
     /**
