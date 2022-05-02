49f0ae787b4a3c645d58ee9de341ffe4b5558e70
Marco Aurelio Cardoso <marcoaurelio.cardoso@gmail.com>
2022-05-02 17:15:53 -0300

    refactor(generic gates): move shared steps to another methods

--- app/Gates/GenericGates.php
+++ app/Gates/GenericGates.php
@@ -7,244 +7,106 @@ use App\Models\User;
 
 class GenericGates
 {
+    private static function checkGenericRoleUta(String $acronym): bool
+    {
+        //return $user->userType->acronym == $acronym;
+
+        //need to have session UserTypeAssignment active.
+        $currentUTA = auth()->user()->getCurrentUTA();
+        if (!$currentUTA) {
+            return false;
+        }
+
+        //if currentUTA (UserTypeAssignment) is $acronym, ok
+        $acronymMatch = $currentUTA->userType->acronym === $acronym;
+        if ($acronymMatch) {
+            return true;
+        }
+
+        //if no permission
+        return false;
+    }
+
+    private static function checkGlobalRoleUta(String $acronym): bool
+    {
+        //need to have session UserTypeAssignment active.
+        $currentUTA = auth()->user()->getCurrentUTA();
+        if (!$currentUTA) {
+            return false;
+        }
+
+        //if currentUTA (UserTypeAssignment) is $acronym, ok
+        $acronymMatch = $currentUTA->userType->acronym === $acronym;
+        $course_id_null = $currentUTA->course_id === null;
+        if ($acronymMatch && $course_id_null) {
+            return true;
+        }
+
+        //if no permission
+        return false;
+    }
+
     public static function define()
     {
 
         /* define a system admin user role */
         Gate::define('isAdm', function ($user) {
-            //return $user->userType->acronym == 'adm';
-
-            //need to have session UserTypeAssignment active.
-            $currentUTA = auth()->user()->getCurrentUTA();
-            if (!$currentUTA) {
-                return false;
-            }
-
-            //if currentUTA (UserTypeAssignment) is adm, ok
-            $acronym_adm = $currentUTA->userType->acronym === 'adm';
-            if ($acronym_adm) {
-                return true;
-            }
-
-            //if no permission
-            return false;
+            return self::checkGenericRoleUta('adm');
         });
 
         /* define a director user role */
         Gate::define('isDir', function ($user) {
-            //return $user->userType->acronym == 'dir';
-
-            //need to have session UserTypeAssignment active.
-            $currentUTA = auth()->user()->getCurrentUTA();
-            if (!$currentUTA) {
-                return false;
-            }
-
-            //if currentUTA (UserTypeAssignment) is dir, ok
-            $acronym_dir = $currentUTA->userType->acronym === 'dir';
-            if ($acronym_dir) {
-                return true;
-            }
-
-            //if no permission
-            return false;
+            return self::checkGenericRoleUta('dir');
         });
 
         /* define a grantor assistant user role */
         Gate::define('isAss', function ($user) {
-            //return $user->userType->acronym == 'ass';
-
-            //need to have session UserTypeAssignment active.
-            $currentUTA = auth()->user()->getCurrentUTA();
-            if (!$currentUTA) {
-                return false;
-            }
-
-            //if currentUTA (UserTypeAssignment) is ass, ok
-            $acronym_ass = $currentUTA->userType->acronym === 'ass';
-            if ($acronym_ass) {
-                return true;
-            }
-
-            //if no permission
-            return false;
+            return self::checkGenericRoleUta('ass');
         });
 
         /* define a academic secretary user role */
         Gate::define('isSec', function ($user) {
-            //return $user->userType->acronym == 'sec';
-
-            //need to have session UserTypeAssignment active.
-            $currentUTA = auth()->user()->getCurrentUTA();
-            if (!$currentUTA) {
-                return false;
-            }
-
-            //if currentUTA (UserTypeAssignment) is sec, ok
-            $acronym_sec = $currentUTA->userType->acronym === 'sec';
-            if ($acronym_sec) {
-                return true;
-            }
-
-            //if no permission
-            return false;
+            return self::checkGenericRoleUta('sec');
         });
 
         /* define a ldi user role */
         Gate::define('isLdi', function ($user) {
-            //return $user->userType->acronym == 'ldi';
-            //need to have session UserTypeAssignment active.
-            $currentUTA = auth()->user()->getCurrentUTA();
-            if (!$currentUTA) {
-                return false;
-            }
-
-            //if currentUTA (UserTypeAssignment) is gra, ok
-            $acronym_ldi = $currentUTA->userType->acronym === 'ldi';
-            if ($acronym_ldi) {
-                return true;
-            }
-
-            //if no permission
-            return false;
+            return self::checkGenericRoleUta('ldi');
         });
 
         /* define a grantee user role */
         Gate::define('isCoord', function ($user) {
-            //return $user->userType->acronym == 'gra';
-
-            //need to have session UserTypeAssignment active.
-            $currentUTA = auth()->user()->getCurrentUTA();
-            if (!$currentUTA) {
-                return false;
-            }
-
-            //if currentUTA (UserTypeAssignment) is gra, ok
-            $acronym_gra = $currentUTA->userType->acronym === 'coord';
-            if ($acronym_gra) {
-                return true;
-            }
-
-            //if no permission
-            return false;
+            return self::checkGenericRoleUta('coord');
         });
 
         /* define a system admin user role */
         Gate::define('isAdm-global', function (User $user) {
-
-            //need to have session UserTypeAssignment active.
-            $currentUTA = auth()->user()->getCurrentUTA();
-            if (!$currentUTA) {
-                return false;
-            }
-
-            //if currentUTA (UserTypeAssignment) is adm, ok
-            $acronym_adm = $currentUTA->userType->acronym === 'adm';
-            $course_id_null = $currentUTA->course_id === null;
-            if ($acronym_adm && $course_id_null) {
-                return true;
-            }
-
-            //if no permission
-            return false;
+            return self::checkGlobalRoleUta('adm');
         });
 
         /* define a system diretor user role */
         Gate::define('isDir-global', function (User $user) {
-
-            //need to have session UserTypeAssignment active.
-            $currentUTA = auth()->user()->getCurrentUTA();
-            if (!$currentUTA) {
-                return false;
-            }
-
-            //if currentUTA (UserTypeAssignment) is dir, ok
-            $acronym_dir = $currentUTA->userType->acronym === 'dir';
-            $course_id_null = $currentUTA->course_id === null;
-            if ($acronym_dir && $course_id_null) {
-                return true;
-            }
-
-            //if no permission
-            return false;
+            return self::checkGlobalRoleUta('dir');
         });
 
         /* define a system Assistant user role */
         Gate::define('isAss-global', function (User $user) {
-
-            //need to have session UserTypeAssignment active.
-            $currentUTA = auth()->user()->getCurrentUTA();
-            if (!$currentUTA) {
-                return false;
-            }
-
-            //if currentUTA (UserTypeAssignment) is ass, ok
-            $acronym_ass = $currentUTA->userType->acronym === 'ass';
-            $course_id_null = $currentUTA->course_id === null;
-            if ($acronym_ass && $course_id_null) {
-                return true;
-            }
-
-            //if no permission
-            return false;
+            return self::checkGlobalRoleUta('ass');
         });
 
         /* define a system sec user role */
         Gate::define('isSec-global', function (User $user) {
-
-            //need to have session UserTypeAssignment active.
-            $currentUTA = auth()->user()->getCurrentUTA();
-            if (!$currentUTA) {
-                return false;
-            }
-
-            //if currentUTA (UserTypeAssignment) is sec, ok
-            $acronym_sec = $currentUTA->userType->acronym === 'sec';
-            $course_id_null = $currentUTA->course_id === null;
-            if ($acronym_sec && $course_id_null) {
-                return true;
-            }
-
-            //if no permission
-            return false;
+            return self::checkGlobalRoleUta('sec');
         });
 
         /* define a grantee user role */
         Gate::define('isCoord-global', function (User $user) {
-            //need to have session UserTypeAssignment active.
-            $currentUTA = auth()->user()->getCurrentUTA();
-            if (!$currentUTA) {
-                return false;
-            }
-
-            //if currentUTA (UserTypeAssignment) is coord, ok
-            $acronym_coord = $currentUTA->userType->acronym === 'coord';
-            $course_id_null = $currentUTA->course_id === null;
-            if ($acronym_coord && $course_id_null) {
-                return true;
-            }
-
-            //if no permission
-            return false;
+            return self::checkGlobalRoleUta('coord');
         });
 
         /* define a grantee user role */
         Gate::define('isLdi-global', function (User $user) {
-            //need to have session UserTypeAssignment active.
-            $currentUTA = auth()->user()->getCurrentUTA();
-            if (!$currentUTA) {
-                return false;
-            }
-
-            //if currentUTA (UserTypeAssignment) is ldi, ok
-            $acronym_ldi = $currentUTA->userType->acronym === 'ldi';
-            $course_id_null = $currentUTA->course_id === null;
-            if ($acronym_ldi && $course_id_null) {
-                return true;
-            }
-
-            //if no permission
-            return false;
+            return self::checkGlobalRoleUta('ldi');
         });
 
         /* define a coord with course_id */
