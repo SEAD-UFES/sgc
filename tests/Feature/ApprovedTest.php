4cef5361dd51b673c778903c6b5c44856b6016b4
Marco Aurelio Cardoso <marcoaurelio.cardoso@gmail.com>
2022-05-04 17:51:41 -0300

    test(approved): test through web responses

---
+++ tests/Feature/ApprovedTest.php
@@ -0,0 +1,206 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Approved;
+use App\Models\User;
+use App\Models\UserType;
+use App\Models\UserTypeAssignment;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ApprovedTest extends TestCase
+{
+    use RefreshDatabase;
+
+    private static $userAdm;
+    private static $userDir;
+    private static $userAss;
+    private static $userSec;
+    private static $userCoord;
+    private static $userLdi;
+
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        self::$userAdm = User::factory()->create();
+        $userTypeAdm = UserType::factory()->admin()->create();
+        UserTypeAssignment::factory()->create([
+            'user_id' => self::$userAdm->id,
+            'user_type_id' => $userTypeAdm->id,
+            'course_id' => null,
+        ]);
+
+        self::$userDir = User::factory()->create();
+        $userTypeDir = UserType::factory()->director()->create();
+        UserTypeAssignment::factory()->create([
+            'user_id' => self::$userDir->id,
+            'user_type_id' => $userTypeDir->id,
+            'course_id' => null,
+        ]);
+
+        self::$userAss = User::factory()->create();
+        $userTypeAss = UserType::factory()->assistant()->create();
+        UserTypeAssignment::factory()->create([
+            'user_id' => self::$userAss->id,
+            'user_type_id' => $userTypeAss->id,
+            'course_id' => null,
+        ]);
+
+        self::$userSec = User::factory()->create();
+        $userTypeSec = UserType::factory()->secretary()->create();
+        UserTypeAssignment::factory()->create([
+            'user_id' => self::$userSec->id,
+            'user_type_id' => $userTypeSec->id,
+            'course_id' => null,
+        ]);
+
+        self::$userCoord = User::factory()->create();
+        $userTypeCoord = UserType::factory()->coordinator()->create();
+        UserTypeAssignment::factory()->create([
+            'user_id' => self::$userCoord->id,
+            'user_type_id' => $userTypeCoord->id,
+            'course_id' => null,
+        ]);
+
+        self::$userLdi = User::factory()->create();
+        $userTypeLdi = UserType::factory()->ldi()->create();
+        UserTypeAssignment::factory()->create([
+            'user_id' => self::$userLdi->id,
+            'user_type_id' => $userTypeLdi->id,
+            'course_id' => null,
+        ]);
+
+        Approved::factory()->create(
+            [
+                'name' => 'John Doe',
+                'email' => 'john@test.com',
+                'area_code' => '01',
+                'phone' => '12345678',
+                'mobile' => '123456789',
+                'announcement' => '001',
+            ]
+        );
+
+        Approved::factory()->create(
+            [
+                'name' => 'Jane Doe',
+                'email' => 'jane@othertest.com',
+                'area_code' => '02',
+                'phone' => '01234567',
+                'mobile' => '012345678',
+                'announcement' => '002',
+            ]
+        );
+    }
+
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     * @test
+     */
+    public function unloggedUserShouldntSeeBonds()
+    {
+        $response = $this->get('/approveds');
+        $response->assertRedirect(route('auth.login'));
+    }
+
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     * @test
+     */
+    public function administratorShouldSeeBonds()
+    {
+        $this->be(self::$userAdm)
+            ->withSession(['current_uta' => auth()->user()->getFirstUTA(), 'current_uta_id' => auth()->user()->getFirstUTA()->id]);
+
+        $response = $this->get('/approveds');
+        $response->assertSee(['John Doe', 'Jane Doe', 'john@test.com', 'jane@othertest.com']);
+        $response->assertStatus(200);
+    }
+
+
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     * @test
+     */
+    public function directorShouldSeeBonds()
+    {
+        $this->be(self::$userDir)
+            ->withSession(['current_uta' => auth()->user()->getFirstUTA(), 'current_uta_id' => auth()->user()->getFirstUTA()->id]);
+
+        $response = $this->get('/approveds');
+        $response->assertSee(['John Doe', 'Jane Doe', 'john@test.com', 'jane@othertest.com']);
+        $response->assertStatus(200);
+    }
+
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     * @test
+     */
+    public function assistantShouldntSeeBonds()
+    {
+        $this->be(self::$userLdi)
+            ->withSession(['current_uta' => auth()->user()->getFirstUTA(), 'current_uta_id' => auth()->user()->getFirstUTA()->id]);
+
+        $response = $this->get('/approveds');
+        $response->assertStatus(403);
+    }
+
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     * @test
+     */
+    public function secretaryShouldSeeBonds()
+    {
+        $this->be(self::$userSec)
+            ->withSession(['current_uta' => auth()->user()->getFirstUTA(), 'current_uta_id' => auth()->user()->getFirstUTA()->id]);
+
+        $response = $this->get('/approveds');
+        $response->assertSee(['John Doe', 'Jane Doe', 'john@test.com', 'jane@othertest.com']);
+        $response->assertStatus(200);
+    }
+
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     * @test
+     */
+    public function ldiShouldntSeeBonds()
+    {
+        $this->be(self::$userLdi)
+            ->withSession(['current_uta' => auth()->user()->getFirstUTA(), 'current_uta_id' => auth()->user()->getFirstUTA()->id]);
+
+        $response = $this->get('/approveds');
+        $response->assertStatus(403);
+    }
+
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     * @test
+     */
+    public function coordinatorShouldSeeBonds()
+    {
+        $this->be(self::$userCoord)
+            ->withSession(['current_uta' => auth()->user()->getFirstUTA(), 'current_uta_id' => auth()->user()->getFirstUTA()->id]);
+
+        $response = $this->get('/approveds');
+        $response->assertSee(['John Doe', 'Jane Doe', 'john@test.com', 'jane@othertest.com']);
+        $response->assertStatus(200);
+    }
+}
