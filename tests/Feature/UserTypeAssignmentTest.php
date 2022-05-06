edf08852fa7a9c0ba401c61fa56ec5680f1126c7
Marco Aurelio Cardoso <marcoaurelio.cardoso@gmail.com>
2022-05-06 04:41:29 -0300

    test(user type assignment): test through web responses

---
+++ tests/Feature/UserTypeAssignmentTest.php
@@ -0,0 +1,201 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use App\Models\UserType;
+use App\Models\UserTypeAssignment;
+use App\Models\Course;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class UserTypeAssignmentTest extends TestCase
+{
+    use RefreshDatabase;
+
+    private static $userAdm;
+    private static $userDir;
+    private static $userAss;
+    private static $userSec;
+    private static $userCoord;
+    private static $userLdi;
+
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        self::$userAdm = User::factory()->create();
+        $userTypeAdm = UserType::factory()->admin()->create();
+        UserTypeAssignment::factory()->create([
+            'user_id' => self::$userAdm->id,
+            'user_type_id' => $userTypeAdm->id,
+            'course_id' => null,
+        ]);
+
+        self::$userDir = User::factory()->create();
+        $userTypeDir = UserType::factory()->director()->create();
+        UserTypeAssignment::factory()->create([
+            'user_id' => self::$userDir->id,
+            'user_type_id' => $userTypeDir->id,
+            'course_id' => null,
+        ]);
+
+        self::$userAss = User::factory()->create();
+        $userTypeAss = UserType::factory()->assistant()->create();
+        UserTypeAssignment::factory()->create([
+            'user_id' => self::$userAss->id,
+            'user_type_id' => $userTypeAss->id,
+            'course_id' => null,
+        ]);
+
+        self::$userSec = User::factory()->create();
+        $userTypeSec = UserType::factory()->secretary()->create();
+        UserTypeAssignment::factory()->create([
+            'user_id' => self::$userSec->id,
+            'user_type_id' => $userTypeSec->id,
+            'course_id' => null,
+        ]);
+
+        self::$userCoord = User::factory()->create();
+        $userTypeCoord = UserType::factory()->coordinator()->create();
+        UserTypeAssignment::factory()->create([
+            'user_id' => self::$userCoord->id,
+            'user_type_id' => $userTypeCoord->id,
+            'course_id' => null,
+        ]);
+
+        self::$userLdi = User::factory()->create();
+        $userTypeLdi = UserType::factory()->ldi()->create();
+        UserTypeAssignment::factory()->create([
+            'user_id' => self::$userLdi->id,
+            'user_type_id' => $userTypeLdi->id,
+            'course_id' => null,
+        ]);
+
+        UserTypeAssignment::factory()->create(
+            [
+                'user_id' => User::factory()->create(['email' => 'johndoe@test.com']),
+                'user_type_id' => UserType::factory()->create(['name' => 'Type one']),
+                'course_id' => Course::factory()->create(['name' => 'Course Alpha']),
+                'begin' => now(),
+                'end' => now(),
+            ]
+        );
+
+        UserTypeAssignment::factory()->create(
+            [
+                'user_id' => User::factory()->create(['email' => 'janedoe@test2.com']),
+                'user_type_id' => UserType::factory()->create(['name' => 'Type two']),
+                'course_id' => Course::factory()->create(['name' => 'Course Beta']),
+                'begin' => now(),
+                'end' => now(),
+            ]
+        );
+    }
+
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     * @test
+     */
+    public function unloggedUserShouldntSeeUserTypeAssignments()
+    {
+        $response = $this->get('/userTypeAssignments');
+        $response->assertRedirect(route('auth.login'));
+    }
+
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     * @test
+     */
+    public function administratorShouldSeeUserTypeAssignments()
+    {
+        $this->be(self::$userAdm)
+            ->withSession(['current_uta' => auth()->user()->getFirstUTA(), 'current_uta_id' => auth()->user()->getFirstUTA()->id]);
+
+        $response = $this->get('/userTypeAssignments');
+        $response->assertSee(['johndoe@test.com', 'janedoe@test2.com', 'Type one (Course Alpha)', 'Type two (Course Beta)']);
+        $response->assertStatus(200);
+    }
+
+
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     * @test
+     */
+    public function directorShouldntSeeUserTypeAssignments()
+    {
+        $this->be(self::$userDir)
+            ->withSession(['current_uta' => auth()->user()->getFirstUTA(), 'current_uta_id' => auth()->user()->getFirstUTA()->id]);
+
+        $response = $this->get('/userTypeAssignments');
+        $response->assertStatus(403);
+    }
+
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     * @test
+     */
+    public function assistantShouldntSeeUserTypeAssignments()
+    {
+        $this->be(self::$userAss)
+            ->withSession(['current_uta' => auth()->user()->getFirstUTA(), 'current_uta_id' => auth()->user()->getFirstUTA()->id]);
+
+        $response = $this->get('/userTypeAssignments');
+        $response->assertStatus(403);
+    }
+
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     * @test
+     */
+    public function secretaryShouldntSeeUserTypeAssignments()
+    {
+        $this->be(self::$userSec)
+            ->withSession(['current_uta' => auth()->user()->getFirstUTA(), 'current_uta_id' => auth()->user()->getFirstUTA()->id]);
+
+        $response = $this->get('/userTypeAssignments');
+        $response->assertStatus(403);
+    }
+
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     * @test
+     */
+    public function ldiShouldntSeeUserTypeAssignments()
+    {
+        $this->be(self::$userLdi)
+            ->withSession(['current_uta' => auth()->user()->getFirstUTA(), 'current_uta_id' => auth()->user()->getFirstUTA()->id]);
+
+        $response = $this->get('/userTypeAssignments');
+        $response->assertStatus(403);
+    }
+
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     * @test
+     */
+    public function coordinatorShouldntSeeUserTypeAssignments()
+    {
+        $this->be(self::$userCoord)
+            ->withSession(['current_uta' => auth()->user()->getFirstUTA(), 'current_uta_id' => auth()->user()->getFirstUTA()->id]);
+
+        $response = $this->get('/userTypeAssignments');
+        $response->assertStatus(403);
+    }
+}
