a6227106cf374642d3b3dcdfff2a943793ab7829
Marco Aurelio Cardoso <marcoaurelio.cardoso@gmail.com>
2022-06-02 16:39:51 -0300

    fix(employee show): format cpf, phones and cep
    Format CPF, landline and mobile phones and postal code strings

--- resources/views/employee/show.blade.php
+++ resources/views/employee/show.blade.php
@@ -29,7 +29,7 @@
                                 </div>
                                 <div class="mb-2 row">
                                     <div class="col-sm-4 col-lg-3"><strong>CPF:</strong></div>
-                                    <div class="col-sm-8 col-lg-9">{{ $employee->cpf ?? '-' }}
+                                    <div class="col-sm-8 col-lg-9">{{ isset($employee->cpf) ? preg_replace('~(\d{3})(\d{3})(\d{3})(\d{2})~', '$1.$2.$3-$4', $employee->cpf) : '-' }}
                                     </div>
                                 </div>
                                 <div class="mb-2 row">
@@ -122,12 +122,12 @@
                                 </div>
                                 <div class="mb-2 row">
                                     <div class="col-sm-4 col-lg-3"><strong>Telefone:</strong></div>
-                                    <div class="col-sm-8 col-lg-9"><a href='tel:{{ $employee->phone }}'>{{ $employee->phone ?? '-' }}</a>
+                                    <div class="col-sm-8 col-lg-9"><a href='tel:{{ $employee->phone }}'>{{ isset($employee->phone) ? preg_replace('~(\d{2})[^\d]{0,7}(\d{4})[^\d]{0,7}(\d{4})~', '($1) $2-$3', $employee->phone) : '-' }}</a>
                                     </div>
                                 </div>
                                 <div class="mb-2 row">
                                     <div class="col-sm-4 col-lg-3"><strong>Celular:</strong></div>
-                                    <div class="col-sm-8 col-lg-9"><a href='tel:{{ $employee->mobile }}'>{{ $employee->mobile ?? '-' }}</a>
+                                    <div class="col-sm-8 col-lg-9"><a href='tel:{{ $employee->mobile }}'>{{ isset($employee->mobile) ? preg_replace('~(\d{2})[^\d]{0,7}(\d{5})[^\d]{0,7}(\d{4})~', '($1) $2-$3', $employee->mobile) : '-' }}</a>
                                     </div>
                                 </div>
                                 <div class="mb-2 row">
@@ -157,7 +157,7 @@
                                 </div>
                                 <div class="mb-2 row">
                                     <div class="col-sm-4 col-lg-3"><strong>CEP:</strong></div>
-                                    <div class="col-sm-8 col-lg-9">{{ $employee->address_postal_code ?? '-' }}
+                                    <div class="col-sm-8 col-lg-9">{{ isset($employee->address_postal_code) ? preg_replace('~(\d{2})(\d{3})(\d{3})~', '$1$2-$3', $employee->address_postal_code) : '-' }}
                                     </div>
                                 </div>
                                 <div class="mb-2 row">
