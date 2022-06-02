52be180a5c10ddbe3afc15bb7833cc1b4328df51
Marco Aurelio Cardoso <marcoaurelio.cardoso@gmail.com>
2022-06-02 16:51:54 -0300

    fix(employee basic data): format cpf string

--- resources/views/employee/componentBasicEmployeeData.blade.php
+++ resources/views/employee/componentBasicEmployeeData.blade.php
@@ -12,7 +12,7 @@
                 </div>
                 <div class="mb-2 row">
                     <div class="col-sm-4 col-lg-3"><strong>CPF:</strong></div>
-                    <div class="col-sm-8 col-lg-9">{{ $employee->cpf ?? '-' }}</div>
+                    <div class="col-sm-8 col-lg-9">{{ isset($employee->cpf) ? preg_replace('~(\d{3})(\d{3})(\d{3})(\d{2})~', '$1.$2.$3-$4', $employee->cpf) : '-' }}</div>
                 </div>
                 <div class="">
                     @can('employee-show')
