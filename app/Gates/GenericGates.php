9da1bc35491acbc2833d9dab44243a3541441f06
Marco Aurelio Cardoso <marcoaurelio.cardoso@gmail.com>
2022-07-28 15:58:32 -0300

    refactor(generic gates): brake fluent and tight uta test
    brake some fluent calls flux to enable phpstan check
    tight currentUTA test

--- app/Gates/GenericGates.php
+++ app/Gates/GenericGates.php
@@ -3,12 +3,12 @@
 namespace App\Gates;
 
 use App\Models\User;
+use App\Models\UserType;
 use Illuminate\Support\Facades\Gate;
 
 class GenericGates
 {
     /**
-     *
      * @return void
      */
     public static function define(): void
@@ -75,43 +75,70 @@ class GenericGates
 
         /* define a coord with course_id */
         Gate::define('isCoord-course_id', static function (User $user, int $course_id) {
+            /**
+             * @var User $currentUser
+             */
+            $currentUser = auth()->user();
             //need to have session UserTypeAssignment active.
-            $currentUTA = auth()->user()->getCurrentUta();
-            if (! $currentUTA) {
+            $currentUTA = $currentUser->getCurrentUta();
+            if (! $currentUTA instanceof \App\Models\UserTypeAssignment) {
                 return false;
             }
+
+            /**
+             * @var UserType $currentUserType
+             */
+            $currentUserType = $currentUTA->userType;
             //if currentUTA (UserTypeAssignment) is coord, ok
-            $is_coord = $currentUTA->userType->acronym === 'coord';
+            $is_coord = $currentUserType->acronym === 'coord';
             // Issue #36: For some reason, sometimes $course_id isn't integer, but string... Fixed with typed parameter.
             $course_id_match = $currentUTA->course_id === $course_id;
 
             return $is_coord && $course_id_match;
         });
     }
+
     private static function checkGenericRoleUta(string $acronym): bool
     {
         //return $user->userType->acronym == $acronym;
+        /**
+         * @var User $currentUser
+         */
+        $currentUser = auth()->user();
 
         //need to have session UserTypeAssignment active.
-        $currentUTA = auth()->user()->getCurrentUta();
-        if (! $currentUTA) {
+        $currentUTA = $currentUser->getCurrentUta();
+        if (! $currentUTA instanceof \App\Models\UserTypeAssignment) {
             return false;
         }
 
+        /**
+         * @var UserType $currentUserType
+         */
+        $currentUserType = $currentUTA->userType;
+
         //if currentUTA (UserTypeAssignment) is $acronym, ok
-        return $currentUTA->userType->acronym === $acronym;
+        return $currentUserType->acronym === $acronym;
     }
 
     private static function checkGlobalRoleUta(string $acronym): bool
     {
+        /**
+         * @var User $currentUser
+         */
+        $currentUser = auth()->user();
         //need to have session UserTypeAssignment active.
-        $currentUTA = auth()->user()->getCurrentUta();
-        if (! $currentUTA) {
+        $currentUTA = $currentUser->getCurrentUta();
+        if (! $currentUTA instanceof \App\Models\UserTypeAssignment) {
             return false;
         }
 
+        /**
+         * @var UserType $currentUserType
+         */
+        $currentUserType = $currentUTA->userType;
         //if currentUTA (UserTypeAssignment) is $acronym, ok
-        $acronymMatch = $currentUTA->userType->acronym === $acronym;
+        $acronymMatch = $currentUserType->acronym === $acronym;
         $course_id_null = $currentUTA->course_id === null;
 
         return $acronymMatch && $course_id_null;
