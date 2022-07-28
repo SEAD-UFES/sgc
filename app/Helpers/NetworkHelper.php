8454727aa93f204830273dfe676791eccdaee256
Marco Aurelio Cardoso <marcoaurelio.cardoso@gmail.com>
2022-07-28 17:09:20 -0300

    refactor: simplify bool identical true

--- app/Helpers/NetworkHelper.php
+++ app/Helpers/NetworkHelper.php
@@ -8,7 +8,7 @@ class NetworkHelper
     public static function getClientIpAddress(): string
     {
         foreach (['HTTP_CLIENT_IP', 'HTTP_X_FORWARDED_FOR', 'HTTP_X_FORWARDED', 'HTTP_X_CLUSTER_CLIENT_IP', 'HTTP_FORWARDED_FOR', 'HTTP_FORWARDED', 'REMOTE_ADDR'] as $key) {
-            if (array_key_exists($key, $_SERVER) === true) {
+            if (array_key_exists($key, $_SERVER)) {
                 foreach (explode(',', $_SERVER[$key]) as $ip) {
                     return trim($ip);
                 }
