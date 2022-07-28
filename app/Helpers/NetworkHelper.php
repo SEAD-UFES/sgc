ce1024d2fa82b39d0f4a362f7bf98645fa0ff9db
Marco Aurelio Cardoso <marcoaurelio.cardoso@gmail.com>
2022-07-28 16:49:36 -0300

    refactor(network helper): simplify return

--- app/Helpers/NetworkHelper.php
+++ app/Helpers/NetworkHelper.php
@@ -10,8 +10,7 @@ class NetworkHelper
         foreach (['HTTP_CLIENT_IP', 'HTTP_X_FORWARDED_FOR', 'HTTP_X_FORWARDED', 'HTTP_X_CLUSTER_CLIENT_IP', 'HTTP_FORWARDED_FOR', 'HTTP_FORWARDED', 'REMOTE_ADDR'] as $key) {
             if (array_key_exists($key, $_SERVER) === true) {
                 foreach (explode(',', $_SERVER[$key]) as $ip) {
-                    $ip = trim($ip);
-                    return $ip;
+                    return trim($ip);
                 }
             }
         }
