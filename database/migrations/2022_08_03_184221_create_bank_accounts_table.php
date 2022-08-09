972f01726500a6dc50bd03812c5362f8cb031094
Marco Aurelio Cardoso <marcoaurelio.cardoso@gmail.com>
2022-08-09 19:00:23 -0300

    feat(bank account and qualification): migrations

---
+++ database/migrations/2022_08_03_184221_create_bank_accounts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bank_accounts', function (Blueprint $table) {
+            $table->id();
+            $table->string('bank_name')->nullable();
+            $table->string('agency_number')->nullable();
+            $table->string('account_number')->nullable();
+            $table->foreignId('employee_id')->constrained('employees');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bank_accounts');
+    }
+};
---
+++ database/migrations/2022_08_03_185525_create_qualifications_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('qualifications', function (Blueprint $table) {
+            $table->id();
+            $table->string('knowledge_area')->nullable();
+            $table->string('course_name')->nullable();
+            $table->string('institution_name')->nullable();
+            $table->foreignId('bond_id')->constrained('bonds');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('qualifications');
+    }
+};
