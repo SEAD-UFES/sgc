3fc59501fc547a48b736dd8d672c5cc72c287ece
Marco Aurelio Cardoso <marcoaurelio.cardoso@gmail.com>
2022-08-26 18:02:37 -0300

    chore(notice emails): format with a markdown approach

--- resources/views/emails/employeeRegistration/institution-employee-login-created-notice.blade.php
+++ resources/views/emails/employeeRegistration/institution-employee-login-created-notice.blade.php
@@ -6,32 +6,35 @@ Prezad{{ $article }} {{ $receiverName }},
 
 Sirvo-me do momento para informar que o seu login de acesso aos sistemas da Ufes foi criado e que, para a liberação do mesmo, basta criar sua senha de acesso, seguindo as instruções abaixo.
 
-<b>Login Institucional:</b> {{ $receiverInstitutionLogin }}
+**Login Institucional:** {{ $receiverInstitutionLogin }}
 
 Para Criar a nova Senha:
-1. Acessar a página: https://senha.ufes.br/
+1. Acessar a página: <https://senha.ufes.br/>
 2. Clicar no menu 'Senha Única'
 3. Clicar na opção 'Recuperar senha Primeiro acesso'
 4. Ler as instruções apresentadas
 
 Caso haja, futuramente, a necessidade de Alterar a Senha:
-1. Acessar a página: https://senha.ufes.br/
+1. Acessar a página: <https://senha.ufes.br/>
 2. Clicar no menu 'Senha Única'
 3. Clicar na opção 'Alterar Senha'
 4. Ler as instruções apresentadas
 
-<br />
 De posse do login único e senha, você terá acesso à Plataforma dos Cursos EAD, através do seguinte endereço:
 @component('mail::button', ['url' => $lmsUrl, 'color' => 'success'])
 {{ $lmsUrl }}
 @endcomponent
-Função: {{ $receiverRoleName }}<br /><br />
-Para uso do seu E-mail Institucional: {{ $receiverInstitutionEmail }}, <br />basta acessar a página: https://mail.ufes.br.<br />
-<br />
-<br />
-Atenciosamente,<br />
-<br />
-{{ $senderName ?? 'Secretaria Sead' }}<br />
-E-mail: {{ $senderInstitutionalEmail ?? 'secretaria.sead@ufes.br'}}<br />
+
+Função: {{ $receiverRoleName }}
+
+Para uso do seu E-mail Institucional: **{{ $receiverInstitutionEmail }}**,  
+basta acessar a página: <https://mail.ufes.br.>
+
+&nbsp;
+
+Atenciosamente,
+
+{{ $senderName ?? 'Secretaria Sead' }}  
+E-mail: <{{ $senderInstitutionalEmail ?? 'secretaria.sead@ufes.br'}}>  
 Secretaria Acadêmica - Sead/Ufes
 @endcomponent
--- resources/views/emails/employeeRegistration/new-tutor-employment-notice.blade.php
+++ resources/views/emails/employeeRegistration/new-tutor-employment-notice.blade.php
@@ -2,21 +2,29 @@
 $article = $employeeGender->name === 'Masculino' ? 'o' : 'a';
 @endphp
 @component('mail::message')
-Prezada Equipe da Coordenação de Tutoria,<br /><br />
-Sirvo-me do momento para comunicar o cadastramento d{{ $article }} tutor{{ $employeeGender->name === 'Masculino' ? '' : 'a' }} abaixo mencionad{{ $article }} junto ao <b>Curso de {{ $courseName }}</b>.<br /><br />
-Nome: {{ $employeeName }}<br />
-Função: {{ $employeeRoleName }}<br />
+Prezada Equipe da Coordenação de Tutoria,
+
+Sirvo-me do momento para comunicar o cadastramento d{{ $article }} tutor{{ $employeeGender->name === 'Masculino' ? '' : 'a' }} abaixo mencionad{{ $article }} junto ao **Curso de {{ $courseName }}**.
+
+Nome: {{ $employeeName }}  
+Função: {{ $employeeRoleName }}
 @if ($poleName !== 'SEAD')
-Polo: {{ $poleName }}<br />
+<br />Polo: {{ $poleName }}
 @endif
-<br />
-Login de acesso: {{ $employeeInstitutionLogin }}<br /><br />
-E-mail Pessoal: {{ $employeePersonalEmail }}<br />
-E-mail Institucional: {{ $employeeInstitutionEmail }}<br /><br />
-Telefone: {{ isset($employeePhone) ? preg_replace('~(\d{2})[^\d]{0,7}(\d{4})[^\d]{0,7}(\d{4})~', '($1) $2-$3', $employeePhone) : '-' }}<br />
-Celular: {{ isset($employeeMobile) ? preg_replace('~(\d{2})[^\d]{0,7}(\d{5})[^\d]{0,7}(\d{4})~', '($1) $2-$3', $employeeMobile) : '-' }}<br /><br /><br />
-Atenciosamente,<br /><br />
-{{ $senderName ?? 'Secretaria Sead' }}<br />
-E-mail: {{ $senderInstitutionalEmail ?? 'secretaria.sead@ufes.br'}}<br />
+
+Login de acesso: {{ $employeeInstitutionLogin }}
+
+E-mail Pessoal: <{{ $employeePersonalEmail }}>  
+E-mail Institucional: <{{ $employeeInstitutionEmail }}>
+
+Telefone: {{ isset($employeePhone) ? '[' . preg_replace('~(\d{2})[^\d]{0,7}(\d{4})[^\d]{0,7}(\d{4})~', '($1) $2-$3', $employeePhone) . '](tel:' . $employeePhone . ')' : '-' }}  
+Celular: {{ isset($employeeMobile) ? '[' . preg_replace('~(\d{2})[^\d]{0,7}(\d{5})[^\d]{0,7}(\d{4})~', '($1) $2-$3', $employeeMobile) . '](tel:' . $employeeMobile . ')' : '-' }}
+
+&nbsp;
+
+Atenciosamente,
+
+{{ $senderName ?? 'Secretaria Sead' }}  
+E-mail: <{{ $senderInstitutionalEmail ?? 'secretaria.sead@ufes.br'}}>  
 Secretaria Acadêmica - Sead/Ufes
 @endcomponent
