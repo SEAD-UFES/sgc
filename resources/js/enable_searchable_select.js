d9e7595d7e58365e699197c4f7b42c8a4c8c759a
Marco Aurelio Cardoso <marcoaurelio.cardoso@gmail.com>
2022-10-05 17:48:53 -0300

    feature(searchable selects): focus to search box on click

--- resources/js/enable_searchable_select.js
+++ resources/js/enable_searchable_select.js
@@ -4,3 +4,14 @@ $(function () {
         width: '100%',
     });
 });
+
+$(document).on('select2:open', (e) => {
+    const selectId = e.target.id
+
+    $(".select2-search__field[aria-controls='select2-" + selectId + "-results']").each(function (
+        key,
+        value,
+    ){
+        value.focus();
+    })
+})
