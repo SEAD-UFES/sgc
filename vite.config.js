6190a1f167d197fb41982ba4302126bcad90fe43
Marco Aurelio Cardoso <marcoaurelio.cardoso@gmail.com>
2023-08-09 01:39:28 -0300

    chore: convert phpunit and vite conf

--- vite.config.js
+++ vite.config.js
@@ -7,19 +7,19 @@ import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
     plugins: [
-        laravel([
-            // 'resources/css/app.css',
-            'resources/js/app.js',
-            'resources/scss/app.scss',
-            'resources/js/enable_searchable_select.ts',
-            'resources/js/enable_tooltip.ts',
-            'resources/js/enable_popover.ts',
-            'resources/js/enable_inputmask.ts',
-            'resources/js/enable_custom_inputmask.ts'
-        ],
-            {
-                refresh: true,
-            }),
+        laravel({
+            input: [
+                // 'resources/css/app.css',
+                'resources/js/app.js',
+                'resources/scss/app.scss',
+                'resources/js/enable_searchable_select.ts',
+                'resources/js/enable_tooltip.ts',
+                'resources/js/enable_popover.ts',
+                'resources/js/enable_inputmask.ts',
+                'resources/js/enable_custom_inputmask.ts'
+            ],
+            refresh: true,
+        }),
 
         checker({
             typescript: true,
